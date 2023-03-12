0e06be4c6d15a8be79e2403cf9a57620c747309b
Gani Ozturk <ganioztrk@gmail.com>
2023-03-12 15:51:51 +0300

    CarPark

---
+++ Proje2-RentACar-MVC-CodeFirst/RentACar.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RentACar.EntityLayer.Concrete;
+
+namespace RentACar.DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }  // pluralize yapısı - sınıf ismiyle tablo ismi birbirine karışmasın diye boyle bir metot uygulanır. category sınıf ismini simgelerken çoğul hali tablo ismini simgeler
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Car> Cars { get; set; }
+
+    }
+}
