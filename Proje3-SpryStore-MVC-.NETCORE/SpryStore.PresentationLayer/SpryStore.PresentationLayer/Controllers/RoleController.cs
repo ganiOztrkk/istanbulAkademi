452469fb27d2472c834252a17bd1613945b0e7ce
Gani Ozturk <ganioztrk@gmail.com>
2023-04-04 21:32:18 +0300

    .

--- Proje3-SpryStore-MVC-.NETCORE/SpryStore.PresentationLayer/SpryStore.PresentationLayer/Controllers/RoleController.cs
+++ Proje3-SpryStore-MVC-.NETCORE/SpryStore.PresentationLayer/SpryStore.PresentationLayer/Controllers/RoleController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SpryStore.EntityLayer.Concrete;
+using SpryStore.PresentationLayer.Models;
 
 namespace SpryStore.PresentationLayer.Controllers
 {
@@ -14,10 +16,55 @@ namespace SpryStore.PresentationLayer.Controllers
             _roleManager = roleManager;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             var values = _roleManager.Roles.ToList();
             return View(values);
         }
+        [HttpGet]
+        public IActionResult AddRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async  Task<IActionResult> AddRole(AddRoleViewModel addRoleViewModel)
+        {
+            AppRole appRole = new AppRole()
+            {
+                Name = addRoleViewModel.RoleName
+            };
+            await _roleManager.CreateAsync(appRole);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            var values = _roleManager.Roles.FirstOrDefault(y => y.Id == id);
+            await _roleManager.DeleteAsync(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateRole(int id)
+        {
+            var value = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
+            UpdateRoleViewModel model = new UpdateRoleViewModel()
+            {
+                RoleId = value.Id,
+                RoleName = value.Name
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateRole(UpdateRoleViewModel updateRoleViewModel)
+        {
+            var value = _roleManager.Roles.FirstOrDefault(x => x.Id == updateRoleViewModel.RoleId);
+            value.Name = updateRoleViewModel.RoleName;
+            await _roleManager.UpdateAsync(value);
+            return RedirectToAction("Index");
+        }
+
     }
 }
---
+++ Proje3-SpryStore-MVC-.NETCORE/SpryStore.PresentationLayer/SpryStore.PresentationLayer/Models/UpdateRoleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SpryStore.PresentationLayer.Models
+{
+    public class UpdateRoleViewModel
+    {
+        public string RoleName { get; set; }
+        public int RoleId { get; set; }
+    }
+}
