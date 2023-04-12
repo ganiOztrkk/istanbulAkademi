f084c0cf08c2075a26301a5f5b46faf59275f3be
Gani Ozturk <ganioztrk@gmail.com>
2023-04-12 13:08:06 +0300

    4th Project Begins

---
+++ Proje4-CommentProject-MVC-.NETCORE/CommentProject/CommentProjectEntityLayer/Concrete/AppUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CommentProject.EntityLayer.Concrete
+{
+    public class AppUser : IdentityUser <int>
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Image { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<Content> Contents { get; set; }
+
+    }
+}
