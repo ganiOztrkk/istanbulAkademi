9821971ef4fd3404e008c64383eccfa09f75fc5e
Gani Ozturk <ganioztrk@gmail.com>
2023-04-28 00:15:07 +0300

    Api Update

--- Proje5-API.Project-core5.0/AkademiPlusApiProject/AkademiPlusApi.PresentationLayer/Startup.cs
+++ Proje5-API.Project-core5.0/AkademiPlusApiProject/AkademiPlusApi.PresentationLayer/Startup.cs
@@ -44,6 +44,15 @@ namespace AkademiPlusApi.PresentationLayer
             services.AddScoped<IActivityService, ActivityManager>();
 
             services.AddControllers();
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("AkademiPlusCors", opts =>
+                {
+                    opts.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AkademiPlusApi.PresentationLayer", Version = "v1" });
@@ -61,6 +70,8 @@ namespace AkademiPlusApi.PresentationLayer
             }
 
             app.UseHttpsRedirection();
+
+            app.UseCors("AkademiPlusCors");
 
             app.UseRouting();
 
