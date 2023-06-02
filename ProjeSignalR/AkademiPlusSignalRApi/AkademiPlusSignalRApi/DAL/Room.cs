23f35a8aa8f34f157b51ec241adfc506eedb16a0
Gani Ozturk <ganioztrk@gmail.com>
2023-06-02 15:44:36 +0300

    signalR - lastUpdate

--- ProjeSignalR/AkademiPlusSignalRApi/AkademiPlusSignalRApi/DAL/Room.cs
+++ ProjeSignalR/AkademiPlusSignalRApi/AkademiPlusSignalRApi/DAL/Room.cs
@@ -4,6 +4,10 @@ namespace AkademiPlusSignalRApi.DAL
 {
     public class Room
     {
+        public Room()
+        {
+            Users = new List<User>();
+        }
         public int RoomID { get; set; }
         public string RoomName { get; set; }
         public List<User> Users { get; set; }
--- ProjeSignalR/AkademiPlusSignalRApi/AkademiPlusSignalRApi/Hubs/MyHub.cs
+++ ProjeSignalR/AkademiPlusSignalRApi/AkademiPlusSignalRApi/Hubs/MyHub.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AkademiPlusSignalRApi.DAL;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 
 namespace AkademiPlusSignalRApi.Hubs
 {
@@ -19,10 +21,23 @@ namespace AkademiPlusSignalRApi.Hubs
             _context = context;
         }
 
-        public async Task sendName(string name)
+        //public async Task sendName(string name)
+        //{
+        //    Names.Add(name);
+        //    await Clients.All.SendAsync("receiveName", name);
+        //}
+        public async Task SendName(string name)
         {
+            if (Names.Count >= RoomCount)
+            {
+                await Clients.Caller.SendAsync("error", $"Bu odada en fazla {RoomCount} kişi olabilir.");
+            }
             Names.Add(name);
             await Clients.All.SendAsync("receiveName", name);
+        }
+        public async Task GetNames()
+        {
+            await Clients.All.SendAsync("receiveNames", Names); // isim listesinin tümünü almak için yazdık.
         }
 
         public override async Task OnConnectedAsync()
@@ -35,6 +50,47 @@ namespace AkademiPlusSignalRApi.Hubs
         {
             ClientCount--;
             await Clients.All.SendAsync("receiveClientCount", ClientCount);
+        }
+
+        public async Task SendNameByGroup(string name, string roomName)
+        {
+            var room = _context.Rooms.Where(x => x.RoomName == roomName).FirstOrDefault();
+            if (room != null)
+            {
+                room.Users.Add(new User()
+                {
+                    UserName = name
+                });
+            }
+            else
+            {
+                var newRoom = new Room(){RoomName = roomName};
+                newRoom.Users.Add(new User(){UserName = name});
+                _context.Rooms.Add(newRoom);
+            }
+
+            await _context.SaveChangesAsync();
+            await Clients.Group(roomName).SendAsync("receiveMessageByGroup", name, room.RoomID);
+        }
+
+        public async Task GetNameByGroup()
+        {
+            var rooms = _context.Rooms.Include(x => x.Users).Select(y=> new
+            {
+                RoomID = y.RoomID,
+                Users = y.Users.ToList()
+            });
+            await Clients.All.SendAsync("receiveNamesByGroup", rooms);
+        }
+
+        public async Task AddToGroup(string roomName) // grup oluşturmak için degil grubun içine birini eklemek için
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+        }
+
+        public async Task RemoveFromGroup(string roomName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
         }
     }
 }
